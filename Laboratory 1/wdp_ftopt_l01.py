8b1c0900d56457d6648b3c5fb6d76d0635f627eb
majsylw <58003896+majsylw@users.noreply.github.com>
2020-01-12 14:39:38 +0100

    laboratory 1st

---
+++ Laboratory 1/wdp_ftopt_l01.py
@@ -0,0 +1,11 @@
+'''
+Napisz program, który będzie wyświetlał na ekranie napis "Witaj świecie!".
+'''
+
+# definicja funkcji main()
+def main():
+    # Wyswietlenie napisu
+    print("Witaj świecie!")
+
+# wywołanie funkcji main
+main()
